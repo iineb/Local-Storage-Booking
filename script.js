732f3665a45c1dfbb01e724c1c7001e718675d07
iineb <60065083+iineb@users.noreply.github.com>
2020-06-08 16:01:49 +0100

    UPDATE: Update UI with localStorage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const seats = document.querySelectorAll('.row. .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 function setMovieData(movieIndex, moviePrice) {
@@ -20,6 +23,21 @@ function updateSelectedCount() {
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 }
 
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
     setMovieData(e.target.selectedIndex, e.target.value);
@@ -32,3 +50,5 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 });
+
+updateSelectedCount();
